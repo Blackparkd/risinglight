6e788766145b157426f9787554f7c0ac3faa8093
Blackparkd <eduardopereira95@hotmail.com>
2025-03-10 21:11:07 +0000

    Added working tests for the histograms

---
+++ tests/mytests/q10.sql
@@ -0,0 +1,127 @@
+CREATE TABLE NATION  (
+    N_NATIONKEY  INT PRIMARY KEY,
+    N_NAME       CHAR(25) NOT NULL,
+    N_REGIONKEY  INT NOT NULL,
+    N_COMMENT    VARCHAR(152)
+);
+
+CREATE TABLE REGION  (
+    R_REGIONKEY  INT PRIMARY KEY,
+    R_NAME       CHAR(25) NOT NULL,
+    R_COMMENT    VARCHAR(152)
+);
+
+CREATE TABLE PART  (
+    P_PARTKEY     INT PRIMARY KEY,
+    P_NAME        VARCHAR(55) NOT NULL,
+    P_MFGR        CHAR(25) NOT NULL,
+    P_BRAND       CHAR(10) NOT NULL,
+    P_TYPE        VARCHAR(25) NOT NULL,
+    P_SIZE        INT NOT NULL,
+    P_CONTAINER   CHAR(10) NOT NULL,
+    P_RETAILPRICE DECIMAL(15,2) NOT NULL,
+    P_COMMENT     VARCHAR(23) NOT NULL
+);
+
+CREATE TABLE SUPPLIER (
+    S_SUPPKEY     INT PRIMARY KEY,
+    S_NAME        CHAR(25) NOT NULL,
+    S_ADDRESS     VARCHAR(40) NOT NULL,
+    S_NATIONKEY   INT NOT NULL,
+    S_PHONE       CHAR(15) NOT NULL,
+    S_ACCTBAL     DECIMAL(15,2) NOT NULL,
+    S_COMMENT     VARCHAR(101) NOT NULL
+);
+
+CREATE TABLE PARTSUPP (
+    PS_PARTKEY     INT NOT NULL,
+    PS_SUPPKEY     INT NOT NULL,
+    PS_AVAILQTY    INT NOT NULL,
+    PS_SUPPLYCOST  DECIMAL(15,2)  NOT NULL,
+    PS_COMMENT     VARCHAR(199) NOT NULL
+    -- PRIMARY KEY (PS_PARTKEY, PS_SUPPKEY)
+);
+
+CREATE TABLE CUSTOMER (
+    C_CUSTKEY     INT PRIMARY KEY,
+    C_NAME        VARCHAR(25) NOT NULL,
+    C_ADDRESS     VARCHAR(40) NOT NULL,
+    C_NATIONKEY   INT NOT NULL,
+    C_PHONE       CHAR(15) NOT NULL,
+    C_ACCTBAL     DECIMAL(15,2)   NOT NULL,
+    C_MKTSEGMENT  CHAR(10) NOT NULL,
+    C_COMMENT     VARCHAR(117) NOT NULL
+);
+
+CREATE TABLE ORDERS (
+    O_ORDERKEY       INT PRIMARY KEY,
+    O_CUSTKEY        INT NOT NULL,
+    O_ORDERSTATUS    CHAR(1) NOT NULL,
+    O_TOTALPRICE     DECIMAL(15,2) NOT NULL,
+    O_ORDERDATE      DATE NOT NULL,
+    O_ORDERPRIORITY  CHAR(15) NOT NULL,  
+    O_CLERK          CHAR(15) NOT NULL, 
+    O_SHIPPRIORITY   INT NOT NULL,
+    O_COMMENT        VARCHAR(79) NOT NULL
+);
+
+CREATE TABLE LINEITEM (
+    L_ORDERKEY      INT NOT NULL,
+    L_PARTKEY       INT NOT NULL,
+    L_SUPPKEY       INT NOT NULL,
+    L_LINENUMBER    INT NOT NULL,
+    L_QUANTITY      DECIMAL(15,2) NOT NULL,
+    L_EXTENDEDPRICE DECIMAL(15,2) NOT NULL,
+    L_DISCOUNT      DECIMAL(15,2) NOT NULL,
+    L_TAX           DECIMAL(15,2) NOT NULL,
+    L_RETURNFLAG    CHAR(1) NOT NULL,
+    L_LINESTATUS    CHAR(1) NOT NULL,
+    L_SHIPDATE      DATE NOT NULL,
+    L_COMMITDATE    DATE NOT NULL,
+    L_RECEIPTDATE   DATE NOT NULL,
+    L_SHIPINSTRUCT  CHAR(25) NOT NULL,
+    L_SHIPMODE      CHAR(10) NOT NULL,
+    L_COMMENT       VARCHAR(44) NOT NULL
+    -- PRIMARY KEY (L_ORDERKEY, L_LINENUMBER)
+);
+COPY CUSTOMER FROM 'tpch-dbgen/tbl/customer.tbl' ( DELIMITER '|' );
+COPY NATION FROM 'tpch-dbgen/tbl/nation.tbl' ( DELIMITER '|' );
+COPY ORDERS FROM 'tpch-dbgen/tbl/orders.tbl' ( DELIMITER '|' );
+COPY PART FROM 'tpch-dbgen/tbl/part.tbl' ( DELIMITER '|' );
+COPY PARTSUPP FROM 'tpch-dbgen/tbl/partsupp.tbl' ( DELIMITER '|' );
+COPY REGION FROM 'tpch-dbgen/tbl/region.tbl' ( DELIMITER '|' );
+COPY SUPPLIER FROM 'tpch-dbgen/tbl/supplier.tbl' ( DELIMITER '|' );
+COPY LINEITEM FROM 'tpch-dbgen/tbl/lineitem.tbl' ( DELIMITER '|' );
+
+select
+    c_custkey,
+    c_name,
+    sum(l_extendedprice * (1 - l_discount)) as revenue,
+    c_acctbal,
+    n_name,
+    c_address,
+    c_phone,
+    c_comment
+from
+    customer,
+    orders,
+    lineitem,
+    nation
+where
+    c_custkey = o_custkey
+    and l_orderkey = o_orderkey
+    and o_orderdate >= date '1993-10-01'
+    and o_orderdate < date '1993-10-01' + interval '3' month
+    and l_returnflag = 'R'
+    and c_nationkey = n_nationkey
+group by
+    c_custkey,
+    c_name,
+    c_acctbal,
+    c_phone,
+    n_name,
+    c_address,
+    c_comment
+order by
+    revenue desc
+limit 20;
---
+++ tests/mytests/q7.sql
@@ -0,0 +1,134 @@
+CREATE TABLE NATION  (
+    N_NATIONKEY  INT PRIMARY KEY,
+    N_NAME       CHAR(25) NOT NULL,
+    N_REGIONKEY  INT NOT NULL,
+    N_COMMENT    VARCHAR(152)
+);
+
+CREATE TABLE REGION  (
+    R_REGIONKEY  INT PRIMARY KEY,
+    R_NAME       CHAR(25) NOT NULL,
+    R_COMMENT    VARCHAR(152)
+);
+
+CREATE TABLE PART  (
+    P_PARTKEY     INT PRIMARY KEY,
+    P_NAME        VARCHAR(55) NOT NULL,
+    P_MFGR        CHAR(25) NOT NULL,
+    P_BRAND       CHAR(10) NOT NULL,
+    P_TYPE        VARCHAR(25) NOT NULL,
+    P_SIZE        INT NOT NULL,
+    P_CONTAINER   CHAR(10) NOT NULL,
+    P_RETAILPRICE DECIMAL(15,2) NOT NULL,
+    P_COMMENT     VARCHAR(23) NOT NULL
+);
+
+CREATE TABLE SUPPLIER (
+    S_SUPPKEY     INT PRIMARY KEY,
+    S_NAME        CHAR(25) NOT NULL,
+    S_ADDRESS     VARCHAR(40) NOT NULL,
+    S_NATIONKEY   INT NOT NULL,
+    S_PHONE       CHAR(15) NOT NULL,
+    S_ACCTBAL     DECIMAL(15,2) NOT NULL,
+    S_COMMENT     VARCHAR(101) NOT NULL
+);
+
+CREATE TABLE PARTSUPP (
+    PS_PARTKEY     INT NOT NULL,
+    PS_SUPPKEY     INT NOT NULL,
+    PS_AVAILQTY    INT NOT NULL,
+    PS_SUPPLYCOST  DECIMAL(15,2)  NOT NULL,
+    PS_COMMENT     VARCHAR(199) NOT NULL
+    -- PRIMARY KEY (PS_PARTKEY, PS_SUPPKEY)
+);
+
+CREATE TABLE CUSTOMER (
+    C_CUSTKEY     INT PRIMARY KEY,
+    C_NAME        VARCHAR(25) NOT NULL,
+    C_ADDRESS     VARCHAR(40) NOT NULL,
+    C_NATIONKEY   INT NOT NULL,
+    C_PHONE       CHAR(15) NOT NULL,
+    C_ACCTBAL     DECIMAL(15,2)   NOT NULL,
+    C_MKTSEGMENT  CHAR(10) NOT NULL,
+    C_COMMENT     VARCHAR(117) NOT NULL
+);
+
+CREATE TABLE ORDERS (
+    O_ORDERKEY       INT PRIMARY KEY,
+    O_CUSTKEY        INT NOT NULL,
+    O_ORDERSTATUS    CHAR(1) NOT NULL,
+    O_TOTALPRICE     DECIMAL(15,2) NOT NULL,
+    O_ORDERDATE      DATE NOT NULL,
+    O_ORDERPRIORITY  CHAR(15) NOT NULL,  
+    O_CLERK          CHAR(15) NOT NULL, 
+    O_SHIPPRIORITY   INT NOT NULL,
+    O_COMMENT        VARCHAR(79) NOT NULL
+);
+
+CREATE TABLE LINEITEM (
+    L_ORDERKEY      INT NOT NULL,
+    L_PARTKEY       INT NOT NULL,
+    L_SUPPKEY       INT NOT NULL,
+    L_LINENUMBER    INT NOT NULL,
+    L_QUANTITY      DECIMAL(15,2) NOT NULL,
+    L_EXTENDEDPRICE DECIMAL(15,2) NOT NULL,
+    L_DISCOUNT      DECIMAL(15,2) NOT NULL,
+    L_TAX           DECIMAL(15,2) NOT NULL,
+    L_RETURNFLAG    CHAR(1) NOT NULL,
+    L_LINESTATUS    CHAR(1) NOT NULL,
+    L_SHIPDATE      DATE NOT NULL,
+    L_COMMITDATE    DATE NOT NULL,
+    L_RECEIPTDATE   DATE NOT NULL,
+    L_SHIPINSTRUCT  CHAR(25) NOT NULL,
+    L_SHIPMODE      CHAR(10) NOT NULL,
+    L_COMMENT       VARCHAR(44) NOT NULL
+    -- PRIMARY KEY (L_ORDERKEY, L_LINENUMBER)
+);
+COPY CUSTOMER FROM 'tpch-dbgen/tbl/customer.tbl' ( DELIMITER '|' );
+COPY NATION FROM 'tpch-dbgen/tbl/nation.tbl' ( DELIMITER '|' );
+COPY ORDERS FROM 'tpch-dbgen/tbl/orders.tbl' ( DELIMITER '|' );
+COPY PART FROM 'tpch-dbgen/tbl/part.tbl' ( DELIMITER '|' );
+COPY PARTSUPP FROM 'tpch-dbgen/tbl/partsupp.tbl' ( DELIMITER '|' );
+COPY REGION FROM 'tpch-dbgen/tbl/region.tbl' ( DELIMITER '|' );
+COPY SUPPLIER FROM 'tpch-dbgen/tbl/supplier.tbl' ( DELIMITER '|' );
+COPY LINEITEM FROM 'tpch-dbgen/tbl/lineitem.tbl' ( DELIMITER '|' );
+
+select
+    supp_nation,
+    cust_nation,
+    l_year,
+    sum(volume) as revenue
+from
+    (
+        select
+            n1.n_name as supp_nation,
+            n2.n_name as cust_nation,
+            extract(year from l_shipdate) as l_year,
+            l_extendedprice * (1 - l_discount) as volume
+        from
+            supplier,
+            lineitem,
+            orders,
+            customer,
+            nation n1,
+            nation n2
+        where
+            s_suppkey = l_suppkey
+            and o_orderkey = l_orderkey
+            and c_custkey = o_custkey
+            and s_nationkey = n1.n_nationkey
+            and c_nationkey = n2.n_nationkey
+            and (
+                (n1.n_name = 'UNITED STATES' and n2.n_name = 'CHINA')
+                or (n1.n_name = 'CHINA' and n2.n_name = 'UNITED STATES')
+            )
+            and l_shipdate between date '1995-01-01' and date '1996-12-31'
+    ) as shipping
+group by
+    supp_nation,
+    cust_nation,
+    l_year
+order by
+    supp_nation,
+    cust_nation,
+    l_year;
